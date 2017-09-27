a0aa50c1993178c9836a5bc034e8055b92d27179
Cagle, Anton <anton.cagle@deancare.com>
2017-09-27 15:36:40 -0500

    cors fix 2

--- server.js
+++ server.js
@@ -7,14 +7,17 @@ var ObjectID = mongodb.ObjectID;
 var REGULATIONS_COLLECTION = "regulation";
 
 var app = express();
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+
 
 // //this is set as the start in the package.json file, but to run gridface homepage alone, 
 // //change the value in package.json to "start: npm .bin/www"
